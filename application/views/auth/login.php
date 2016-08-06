2e184e6bf86a23030e9c711beddbd5cf791d799f
Rizki Herdatullah <Rizki Herdatullah>
2016-08-06 21:02:45 +0700

    Edit Login View

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -34,19 +34,19 @@
 	  <div id="login-page">
 	  	<div class="container">
 	  	
-		      <form class="form-login" action="<?php echo site_url('admin/dashboard') ?>">
-		        <h2 class="form-login-heading">sign in now</h2>
+		      <form class="form-login" action="<?php echo site_url('auth/login') ?>">
+		        <h2 class="form-login-heading">Login</h2>
 		        <div class="login-wrap">
-		            <input type="text" class="form-control" placeholder="User ID" autofocus>
+		            <input type="text" name="identity" class="form-control" placeholder="Username" autofocus>
 		            <br>
-		            <input type="password" class="form-control" placeholder="Password">
+		            <input type="password" name="password" class="form-control" placeholder="Password">
+		             
 		            <label class="checkbox">
 		                <span class="pull-right">
 		                    <a data-toggle="modal" href="login.html#myModal"> Forgot Password?</a>
-		
-		                </span>
+						</span>
 		            </label>
-		            <button class="btn btn-theme btn-block" href="index.html" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+		            <button class="btn btn-theme btn-block" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
 		            <hr>
 		            <div class="registration">
 		                Don't have an account yet?<br/>
